5fd964238692e579ba346c265272c0a648ffe08f
GioC <ijidev321@gmail.com>
2026-07-03 01:19:15 +0200

    feat: add rotation based on player's direction

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,10 +6,16 @@ public class PlayerMovement : MonoBehaviour
     [Header("Component References")]
     [SerializeField]
     private Rigidbody2D rb;
+    [SerializeField]
+    private Quaternion leftRotation;
+    [SerializeField]
+    private Quaternion rightRotation;
 
     [Header("Velocities")]
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float rotationSpeed;
 
     private float horizontal;
 
@@ -22,6 +28,17 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocityX = horizontal * moveSpeed;
+
+        if (rb.linearVelocityX > 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, rightRotation, Time.fixedDeltaTime * rotationSpeed);
+        } else if (rb.linearVelocityX < 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, leftRotation, Time.fixedDeltaTime * rotationSpeed);
+        } else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, Time.fixedDeltaTime * rotationSpeed);
+        }
     }
 
     public void Move(InputAction.CallbackContext ctx)
